2669a5e0861b4d2809c2b470a33b9f95e1e8d815
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-10-13 19:03:22 +0300

    feat(styles): add inter font

---
+++ frontend/src/assets/styles/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter } from 'next/font/google'
+
+export const inter = Inter({
+  preload: true,
+  subsets: ['cyrillic', 'latin'],
+  weight: ['300', '400', '500', '700'],
+  style: 'normal',
+  display: 'swap'
+})
