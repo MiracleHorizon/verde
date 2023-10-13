80dcc3e9e0c56eb2da2e5c8d4398ed6c76f16272
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-10-13 22:49:57 +0300

    feat: add SEO for the not-found.tsx

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from 'next'
 
 import { NotFoundView } from '@components/views/NotFound'
+import { SearchEngineOptimizer } from '@utils/SearchEngineOptimizer'
 
-export const metadata: Metadata = {
+const searchEngineOptimizer = new SearchEngineOptimizer({
   title: 'Страница не найдена'
+})
+const basicMedata = searchEngineOptimizer.getBasicMetaData()
+export const metadata: Metadata = {
+  ...basicMedata
 }
 
 export default function NotFound() {
