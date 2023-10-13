96f5a7c2d777a3a6cd4a6e11aa5b835a8ff0bd18
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-10-14 01:01:08 +0300

    feat: add stylelint

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,24 @@
+const StylelintPlugin = require('stylelint-webpack-plugin')
+
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  webpack: config => {
+    config.plugins.push(
+      new StylelintPlugin({
+        configFile: '.stylelintrc.json',
+        context: 'src',
+        files: '**/*.scss',
+        fix: false,
+        quiet: true,
+        failOnError: true
+      })
+    )
+
+    return config
+  }
 }
 
 module.exports = nextConfig
