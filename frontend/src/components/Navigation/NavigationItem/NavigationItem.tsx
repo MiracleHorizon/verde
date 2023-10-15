9a68c58a6dfdf1e0d4c6f691e02c171fe7b80015
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-10-15 22:30:58 +0300

    chore: add React.memo for the NavigationItem

--- frontend/src/components/Navigation/NavigationItem/NavigationItem.tsx
+++ frontend/src/components/Navigation/NavigationItem/NavigationItem.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useMemo } from 'react'
+import { memo, useMemo } from 'react'
 import { useParams } from 'next/navigation'
 import Link from 'next/link'
 import cn from 'classnames'
@@ -9,7 +9,7 @@ import { getCategoryRoute } from '@helpers/getCategoryRoute.ts'
 import type { NavigationCategory } from '@shared/@types/NavigationCategory.ts'
 import styles from './NavigationItem.module.scss'
 
-export function NavigationItem({ id, title, imagePath }: NavigationCategory) {
+function NavigationItem({ id, title, imagePath }: NavigationCategory) {
   const params = useParams()
   const selected = params.id === id.toString()
 
@@ -33,3 +33,7 @@ export function NavigationItem({ id, title, imagePath }: NavigationCategory) {
     </li>
   )
 }
+
+const MemoizedNavigationItem = memo(NavigationItem)
+
+export { MemoizedNavigationItem as NavigationItem }
