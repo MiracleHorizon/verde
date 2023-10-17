33053e5d48da0235bee35ba8c39bd48047d207fb
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-10-17 21:23:47 +0300

    feat: add NumberFormatter utility

---
+++ frontend/src/utils/NumberFormatter/NumberFormatter.ts
@@ -0,0 +1,36 @@
+import { Currency, Locale } from './types.ts'
+
+export class NumberFormatter {
+  private readonly locale: Locale
+  private readonly currency: Currency
+
+  constructor(locale: Locale) {
+    this.locale = locale
+
+    switch (locale) {
+      case Locale.RU:
+        this.currency = Currency.RUB
+        break
+      case Locale.EN:
+        this.currency = Currency.USD
+        break
+    }
+  }
+
+  public formatCurrency(
+    value: number,
+    options?: Omit<Intl.NumberFormatOptions, 'style' | 'currency'>
+  ): string {
+    if (isNaN(value)) {
+      return ''
+    }
+
+    const numberFormat = Intl.NumberFormat(this.locale, {
+      style: 'currency',
+      currency: this.currency,
+      ...options
+    })
+
+    return numberFormat.format(value)
+  }
+}
---
+++ frontend/src/utils/NumberFormatter/index.ts
@@ -0,0 +1,2 @@
+export { NumberFormatter } from './NumberFormatter.ts'
+export { Locale, CurrencySign } from './types.ts'
