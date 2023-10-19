df2ac1f410e7c0fa8082eddab44d686a46325fa6
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-10-19 18:09:45 +0300

    docs: add docs to the getVariantStyles

--- frontend/src/components/ProductCard/helpers/getVariantStyles.ts
+++ frontend/src/components/ProductCard/helpers/getVariantStyles.ts
@@ -2,6 +2,12 @@ import cn from 'classnames'
 
 import type { ProductCardVariant } from '@components/ProductCard'
 
+/**\
+ *
+ * @param styles - CSS-modules class list
+ * @param variant - variant of the ProductCard style
+ * @param classPrefix - additional class selector prefix
+ */
 export function getVariantStyles(
   styles: Record<string, string>,
   variant?: ProductCardVariant,
