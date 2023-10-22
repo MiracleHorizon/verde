4dc52cddb2dbc79b22d09b06d964ebfcfce60b37
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-10-22 22:05:16 +0300

    chore: update fonts display

--- frontend/src/assets/styles/fonts.ts
+++ frontend/src/assets/styles/fonts.ts
@@ -1,13 +1,14 @@
 import { Inter, Roboto } from 'next/font/google'
 
 export const inter = Inter({
-  preload: false,
+  display: 'swap',
   subsets: ['cyrillic'],
   weight: ['400', '500', '600', '700']
 })
 
 export const roboto = Roboto({
   preload: false,
+  display: 'swap',
   subsets: ['cyrillic'],
   weight: ['500', '700']
 })
