45190b0596dc99eb8afbd6b05b455912900b026c
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-11-04 19:48:24 +0300

    chore: add auth inputs blur with Escape

--- apps/frontend/src/app/auth/components/AuthForm/AuthFormInput/AuthFormInput.tsx
+++ apps/frontend/src/app/auth/components/AuthForm/AuthFormInput/AuthFormInput.tsx
@@ -1,23 +1,37 @@
-import { useId } from 'react'
+import { useId, useRef } from 'react'
+import { useEventListener } from 'usehooks-ts'
 import cn from 'classnames'
 
 import type { FieldError, UseFormRegisterReturn } from 'react-hook-form'
 
 import { IconExclamationTriangle } from '@ui/icons/IconExclamationTriangle'
+import { KeyboardCode } from '@enums/KeyboardCode'
 import type { InputProps } from '@interfaces/InputProps'
 import styles from './AuthFormInput.module.scss'
 
 export function AuthFormInput<T extends string>({
-  register,
+  register: { ref, ...register },
   error,
   ...inputAttributes
 }: Props<T>) {
   const id = useId()
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  const handleMount = (node: HTMLInputElement | null) => {
+    ref(node)
+    inputRef.current = node
+  }
+
+  useEventListener('keydown', ev => {
+    if (ev.code !== KeyboardCode.ESCAPE) return
+    inputRef.current?.blur()
+  })
 
   return (
     <label htmlFor={id} className={styles.root}>
       <input
         id={id}
+        ref={handleMount}
         className={cn(
           styles.field,
           error && error.message !== ''
